70252de7a3f02a76add2a1534e097967ec9ed7d4
AndreaPerrelli <snake09031999@gmail.com>
2023-10-29 23:09:16 +0100

    implementing first version of chess.js

--- script.js
+++ script.js
@@ -1,66 +1,49 @@
-// Importa la libreria Chess.js
-const Chess = require('chess.js');
+const boardElement = document.querySelector('.board');
+const game = new Chess(); // Crea una nuova partita con chess.js
+let selectedSquare = null;
 
-// Crea una nuova istanza del gioco di scacchi
-const game = new Chess();
+function renderBoard() {
+    boardElement.innerHTML = ''; // Pulisci la scacchiera
 
-// Funzione per creare la scacchiera
-function creaScacchiera() {
-    const scacchiera = document.querySelector('.scacchiera');
-    
-    for (let fila = 0; fila < 8; fila++) {
-        for (let colonna = 0; colonna < 8; colonna++) {
-            const casella = document.createElement('div');
-            casella.classList.add((fila + colonna) % 2 === 0 ? 'bianco' : 'nero');
-            casella.dataset.fila = fila;
-            casella.dataset.colonna = colonna;
-            casella.addEventListener('click', gestisciClic);
-            scacchiera.appendChild(casella);
-        }
-    }
-}
+    const squares = game.fen().split(' ')[0].split('/').reverse().join('/').split('/').map(row => row.split(''));
 
-// Funzione per gestire il clic su una casella
-function gestisciClic(event) {
-    const fila = parseInt(event.target.dataset.fila);
-    const colonna = parseInt(event.target.dataset.colonna);
-    const posizione = `${String.fromCharCode(97 + colonna)}${8 - fila}`;
-    
-    // Esempio: muovi una pedina
-    const mossa = game.move({
-        from: 'e2',
-        to: posizione,
-        promotion: 'q', // Specifica la promozione (es. regina)
-    });
-    
-    // Aggiorna la scacchiera
-    aggiornaScacchiera();
-    
-    // Verifica se la partita è finita
-    if (game.game_over()) {
-        alert('La partita è finita!');
-    }
-}
-
-// Funzione per aggiornare la scacchiera sulla base dello stato di gioco
-function aggiornaScacchiera() {
-    const scacchiera = document.querySelector('.scacchiera');
-    const caselle = scacchiera.querySelectorAll('div');
-    
-    caselle.forEach(casella => {
-        const fila = parseInt(casella.dataset.fila);
-        const colonna = parseInt(casella.dataset.colonna);
-        const posizione = `${String.fromCharCode(97 + colonna)}${8 - fila}`;
-        const pezzo = game.get(posizione);
-        
-        if (pezzo === null) {
-            casella.textContent = '';
-        } else {
-            casella.textContent = pezzo.type;
-        }
+    squares.forEach((row, rowIndex) => {
+        row.forEach((piece, colIndex) => {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            if ((rowIndex + colIndex) % 2 === 0) {
+                square.classList.add('white');
+            } else {
+                square.classList.add('black');
+            }
+            if (piece !== '-') {
+                const pieceElement = document.createElement('div');
+                pieceElement.classList.add('piece');
+                pieceElement.textContent = pieces[piece];
+                square.appendChild(pieceElement);
+            }
+            square.dataset.square = String.fromCharCode(97 + colIndex) + (8 - rowIndex); // ad es. "e2"
+            boardElement.appendChild(square);
+        });
     });
 }
 
-// Inizializza la scacchiera
-creaScacchiera();
-aggiornaScacchiera();
+boardElement.addEventListener('click', e => {
+    const clickedSquare = e.target.closest('.square').dataset.square;
+    const clickedPiece = game.get(clickedSquare);
+
+    if (selectedSquare) {
+        // Prova a muovere il pezzo
+        const move = game.move({ from: selectedSquare, to: clickedSquare });
+
+        if (move) {
+            renderBoard(); // Aggiorna la scacchiera se la mossa è valida
+        }
+
+        selectedSquare = null;
+    } else if (clickedPiece) {
+        selectedSquare = clickedSquare; // Seleziona il pezzo se è valido
+    }
+});
+
+renderBoard(); // Inizializza la scacchiera
